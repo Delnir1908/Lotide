f0bbcb40da74ccc24aeb55a4391d9a99bbe10f44
Jason Chen <windson7@gmail.com>
2025-02-22 11:20:37 -0800

    new assert function initiaed

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [` + actual + `] === [` + expected + `]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [` + actual + `] !== [` + expected + `]`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(5, 8);
+assertEqual('today is a good day', 'today is a good day');
+assertEqual('apple', 'orange');
+assertEqual(8964, 8964);
+
+
