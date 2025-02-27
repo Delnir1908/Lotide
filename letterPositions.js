7e49809c568af19db66a660ca1f620bc33e4eb1d
Jason Chen <windson7@gmail.com>
2025-02-26 22:53:59 -0800

    letter positions function added, very hard!

---
+++ letterPositions.js
@@ -0,0 +1,49 @@
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+  
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+
+  for (const letter of sentence) {
+
+    if (/^[A-Za-z]$/.test(letter)) {
+      results[letter] = [];
+      for (const index in sentence) {
+        if (letter === sentence[index] && !results[letter].includes(Number(index))) {
+          results[letter].push(Number(index));
+        }
+      }
+    }
+  }
+
+  return results;
+  
+};
+
+let positions = letterPositions('hello world');
+console.log(positions);
+assertArraysEqual(positions['h'],[0]);
+assertArraysEqual(positions['o'],[4,7]);
