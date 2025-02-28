a806bb29d8097e0a7df0d085b6c21057ae9e1d11
Jason Chen <windson7@gmail.com>
2025-02-27 21:51:16 -0800

    find value by key function added, ez

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const findKeyByValue = function(object, string) {
+
+  for (const key in object) {
+    if (object[key] === string) {
+      return key;
+    }
+  }
+
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
