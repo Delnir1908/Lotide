e8239b728b25b6335b4779fc93a9ce6b2f3da991
Jason Chen <windson7@gmail.com>
2025-02-27 23:49:05 -0800

    print function for comparing objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,88 @@
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  
+  return true;
+
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+
+  let key1Array = Object.keys(object1);
+  let key2Array = Object.keys(object2);
+
+  if ((key1Array.length !== key2Array.length)) {
+    return false;
+  }
+
+  for (const key of key1Array) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+
+};
+
+const assertObjectssEqual = function(actual, expected) {
+
+  const inspect = require("util").inspect; 
+
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(actual)}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} !== ${inspect(actual)}`);
+  }
+  
+};
+
+
+
+
+
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertObjectssEqual(multiColorShirtObject, anotherMultiColorShirtObject) // => true
+
+const longSleeveMultiColorShirtObject = {
+  size: "medium",
+  colors: ["red", "blue"],
+  sleeveLength: "long",
+};
+assertObjectssEqual(multiColorShirtObject, longSleeveMultiColorShirtObject); // => false
+
+const car1 = { make: "tesla", type: "sedan" };
+const car2 = { type: "sedan", make: "tesla" };
+const car3 = {awd: true, type: "sedan", make: "tesla"};
+const car4 = { make: "tesla", color: "sedan" };
+assertObjectssEqual(car1, car2);
+assertObjectssEqual(car2, car3);
+assertObjectssEqual(car1, car4);
