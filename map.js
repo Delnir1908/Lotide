60ae025583714b4c292c679d6bb417c4617e3c50
Jason Chen <windson7@gmail.com>
2025-03-01 17:54:55 -0800

    map function added

---
+++ map.js
@@ -0,0 +1,47 @@
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+  
+};
+
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+
+console.log(map(words, (word) => word[0]));
+
+// FUNCTION IMPLEMENTATION
+
+
+const fruits = ['apple', 'banana', 'cherry', 'date']
+assertArraysEqual(map(fruits, (word) => word.length), [5, 6, 6, 4]);
+assertArraysEqual(map(fruits, (word) => word.slice(1,3)), ['pp', 'an', 'he', 'at']);
+assertArraysEqual(map(fruits, (word) => word.toUpperCase()), ['APPLE', 'BANANA', 'CHERRY', 'DATE']);
