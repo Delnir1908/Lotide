62ea5f28cfe973d05a001e86c922465684657623
Jason Chen <windson7@gmail.com>
2025-03-09 18:57:59 -0700

    refactored code with AI feedback

--- eqObjects.js
+++ eqObjects.js
@@ -65,18 +65,16 @@ const eqObjects = function(object1, object2) {
     }
 
   for (const key in object1) {
-    if (typeof(object1[key]) !== typeof(object2[key])) {
-      return false;
+    if (Array.isArray(object1[key])){
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
     } else if (typeof(object1[key]) === 'object') {
       if (!eqObjects(object1[key], object2[key])) {
         return false;
       }
-    } else if (Array.isArray(object1[key])){
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
     } else if (object1[key] !== object2[key]) {
-      return false;
+      return false;ß
     }
   }
 
