31edbcd35d5c9b1cd146b819ecb07d014876eaf9
Jason Chen <windson7@gmail.com>
2025-03-09 18:53:03 -0700

    added checks for nested objects with recursion

--- eqObjects.js
+++ eqObjects.js
@@ -23,7 +23,7 @@ const assertEqual = function(actual, expected) {
 };
 
 
-
+/*
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
@@ -31,7 +31,7 @@ const eqObjects = function(object1, object2) {
   let key1Array = Object.keys(object1);
   let key2Array = Object.keys(object2);
 
-  if ((key1Array.length !== key2Array.length)) {
+  if (key1Array.length !== key2Array.length) {
     return false;
   }
 
@@ -50,6 +50,41 @@ const eqObjects = function(object1, object2) {
   return true;
 
 };
+*/
+
+//base case single level of object and compare
+//nested objects call own function
+
+const eqObjects = function(object1, object2) {
+  
+    let key1Array = Object.keys(object1);
+    let key2Array = Object.keys(object2);
+  
+    if (key1Array.length !== key2Array.length) {
+      return false;
+    }
+
+  for (const key in object1) {
+    if (typeof(object1[key]) !== typeof(object2[key])) {
+      return false;
+    } else if (typeof(object1[key]) === 'object') {
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (Array.isArray(object1[key])){
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+
+};
+
+
 
 
 
