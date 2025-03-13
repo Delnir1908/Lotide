a4b6c3d7256f847161ffa41e74485c5e706276cc
Jason Chen <windson7@gmail.com>
2025-03-12 20:21:55 -0700

    added headTest.js

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -7,3 +7,6 @@ assertEqual('today is a good day', 'today is a good day');
 assertEqual('apple', 'orange');
 assertEqual(8964, 8964);
 
+
+assertEqual((head([2, 4 ,6 ,8])), 2);
+assertEqual((head(['apple', 'orange', 'strawberry', 'banana'])), 'apple');
---
+++ test/headTest.js
@@ -0,0 +1,4 @@
+const assertEqual = require("../assertEqual");
+
+assertEqual((head([2, 4 ,6 ,8])), 2);
+assertEqual((head(['apple', 'orange', 'strawberry', 'banana'])), 'apple');
