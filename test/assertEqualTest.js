10a01dfdad47d17d6607495ba45cf4cfbb274f6e
Jason Chen <windson7@gmail.com>
2025-03-12 20:14:30 -0700

    test file added

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require("../assertEqual");
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(5, 8);
+assertEqual('today is a good day', 'today is a good day');
+assertEqual('apple', 'orange');
+assertEqual(8964, 8964);
+
