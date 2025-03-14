a68a8b0b22f01cc14a6bdcbc64f7e60077132b0c
Jason Chen <windson7@gmail.com>
2025-03-13 22:24:55 -0700

    map added to index

--- map.js
+++ map.js
@@ -1,32 +1,30 @@
-// FUNCTION IMPLEMENTATION
-const eqArrays = function(arr1, arr2) {
+// // FUNCTION IMPLEMENTATION
+// const eqArrays = function(arr1, arr2) {
 
-  if (arr1.length !== arr2.length) {
-    return false;
-  } 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   } 
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
 
-  return true;
+//   return true;
 
-};
+// };
 
-const assertArraysEqual = function(actual, expected) {
+// const assertArraysEqual = function(actual, expected) {
 
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
+//   if (eqArrays(actual, expected)) {
+//     console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+//   } else {
+//     console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+//   }
   
-};
+// };
 
-
-
-const words = ["ground", "control", "to", "major", "tom"];
+// const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function (array, callback) {
   const results = [];
@@ -36,13 +34,14 @@ const map = function (array, callback) {
   return results;
 };
 
+module.exports = map;
 
-console.log(map(words, (word) => word[0]));
+// console.log(map(words, (word) => word[0]));
 
-// FUNCTION IMPLEMENTATION
+// // FUNCTION IMPLEMENTATION
 
 
-const fruits = ['apple', 'banana', 'cherry', 'date']
-assertArraysEqual(map(fruits, (word) => word.length), [5, 6, 6, 4]);
-assertArraysEqual(map(fruits, (word) => word.slice(1,3)), ['pp', 'an', 'he', 'at']);
-assertArraysEqual(map(fruits, (word) => word.toUpperCase()), ['APPLE', 'BANANA', 'CHERRY', 'DATE']);
+// const fruits = ['apple', 'banana', 'cherry', 'date']
+// assertArraysEqual(map(fruits, (word) => word.length), [5, 6, 6, 4]);
+// assertArraysEqual(map(fruits, (word) => word.slice(1,3)), ['pp', 'an', 'he', 'at']);
+// assertArraysEqual(map(fruits, (word) => word.toUpperCase()), ['APPLE', 'BANANA', 'CHERRY', 'DATE']);
