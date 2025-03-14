678f8a5c1ba1b90053abd1e7cd5253fe5fbe5627
Jason Chen <windson7@gmail.com>
2025-03-13 21:30:28 -0700

    talTest and middleTest modified with chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,35 @@
-const assertArraysEqual = require("../assertArraysEqual");
+//const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
-
-
+const assert = require("chai").assert;
+/*
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle(['Lighthouse', 'Labs', 'Coding', 'Finance', 'Chemistry']), ['Coding']);
 assertArraysEqual(middle([10, 89, '6', 23]), [89, '6']);
 assertArraysEqual(middle([1, 10]), []);
 assertArraysEqual(middle([1]), []);
+*/
+describe("#middle", () => {
+ 
+  it(`returns [2, 3] for [1, 2, 3, 4]`, () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns ['Coding'] for ['Lighthouse', 'Labs', 'Coding', 'Finance', 'Chemistry']", () => {
+    assert.deepEqual(middle(['Lighthouse', 'Labs', 'Coding', 'Finance', 'Chemistry']), ['Coding']); 
+  });
+ 
+  it(`returns [89, '6'] for [10, 89, '6', 23]`, () => {
+    assert.deepEqual(middle([10, 89, '6', 23]), [89, '6']);
+  });
+ 
+  it(`returns [] for [1,10]`, () => {
+    assert.deepEqual(middle([1, 10]), []);
+  });
+ 
+  it(`returns [] for [1]`, () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+ 
+});
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,21 @@
 const tail = require("../tail");
+const assert = require("chai").assert;
+/*
 const assertEqual = require("../assertEqual");
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+*/
+describe("#tail", () => {
+
+  it(`returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]`, () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("returns ['5'] for [5, '5']", () => {
+    assert.deepEqual(tail([5, '5']), ['5']); 
+  });
+
+
+});
