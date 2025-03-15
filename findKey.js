3155d461e55064e7fe2504f130c476f8490951f7
Jason Chen <windson7@gmail.com>
2025-03-15 12:21:32 -0700

    findKey code refactored and saved

--- findKey.js
+++ findKey.js
@@ -1,10 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+//   } else {
+//     console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+//   }
+// };
 
 const findKey = function(object, callback) {
 
@@ -16,26 +16,27 @@ const findKey = function(object, callback) {
 
 };
 
-assertEqual(findKey(
-  {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
-  },
-  (x) => x.stars === 2
-), 'noma');
+module.exports = findKey;
+// assertEqual(findKey(
+//   {
+//     "Blue Hill": { stars: 1 },
+//     Akaleri: { stars: 3 },
+//     noma: { stars: 2 },
+//     elBulli: { stars: 3 },
+//     Ora: { stars: 2 },
+//     Akelarre: { stars: 3 },
+//   },
+//   (x) => x.stars === 2
+// ), 'noma');
 
-assertEqual(findKey(
-  {
-    'toyota': { country: 'japan' },
-    'volswagon': { country: 'germany' },
-    'ford': { country: 'usa' },
-    'subaru': { country: 'japan' },
-    'rolls-royce': { country: 'uk' },
-    'ferrari': { country: 'italy' },
-  },
-  (x) => x.country === 'italy'
-), 'ferrari');
+// assertEqual(findKey(
+//   {
+//     'toyota': { country: 'japan' },
+//     'volswagon': { country: 'germany' },
+//     'ford': { country: 'usa' },
+//     'subaru': { country: 'japan' },
+//     'rolls-royce': { country: 'uk' },
+//     'ferrari': { country: 'italy' },
+//   },
+//   (x) => x.country === 'italy'
+// ), 'ferrari');
